22b8347ded760943db510e5abcf037cdf325ba0a
James Benner <james.benner@gmail.com>
2015-06-04 05:01:53 -0400

    Fixed `ADR` and `BDAY` props.

--- jcard2vcard.js
+++ jcard2vcard.js
@@ -13,6 +13,13 @@ var source = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 var jcard = JSON.parse(source).pop();
 ///console.log(jcard);
 
+/**
+ * vCard:
+ * @see http://tools.ietf.org/html/rfc6350
+ *
+ * jCard:
+ * @see http://tools.ietf.org/html/rfc7095
+ */
 var vcard = [ 'BEGIN:VCARD' ];
 
 jcard.forEach(function (item) {
@@ -22,23 +29,30 @@ jcard.forEach(function (item) {
     var val   = item[3];
     //console.log(prop.toUpperCase());
     ///if (Object.keys(param).length === 0) console.log('nope');
-    ///else console.log('yep');
 
     var line = `${prop}:`;
 
-    if (type !== 'text' && !(prop === 'LANG' && type === 'language-tag') &&
+    /**
+     * > If the property's value type is the default type for that property, no
+     * > "VALUE" parameter is included.
+     */
+    if (prop === 'TEL') {
+        line += `VALUE=${type};TYPE="${param.type.join(',')}":`;
+    } else if (prop === 'RELATED') {
+        line += `TYPE=${param.type};`;
+    } else if (type !== 'text' &&
+        !(prop === 'BDAY' && type === 'date-and-or-time') &&
+        !(prop === 'LANG' && type === 'language-tag') &&
         !(prop === 'REV' && type === 'timestamp')
     ) {
         line += `TYPE=${type};`;
-    } else if (prop === 'RELATED') {
-        line += `TYPE=${param.type};`;
     }
 
-    line += (prop === 'N') ? val.join(';') : val;
+    line += (prop === 'N' || prop === 'ADR') ? val.join(';') : val;
     vcard.push(line);
 });
 
 vcard[vcard.length] = 'VCARD:END';
 
 console.log(vcard.join(CRLF));
-console.log(dateTime());
+console.log('\n-----\n\n> @' + dateTime());
