be5e96eca65335770232d3c7d973d8413162e515
James Benner <james.benner@gmail.com>
2016-03-17 19:07:48 -0400

    Removed un-needed comment.

--- src/lib/is-valid-octet-size.js
+++ src/lib/is-valid-octet-size.js
@@ -11,12 +11,3 @@ module.exports = (line) => {
 
     return (Buffer.byteLength(line) <= MAX_OCTETS);
 };
-
-/**
- * Normative References
- * --------------------
- *
- * [RFC6350] Perreault, S., "vCard Format Specification", RFC 6350,
- *           DOI 10.17487/RFC6350, August 2011,
- *           <http://www.rfc-editor.org/info/rfc6350>.
- */
