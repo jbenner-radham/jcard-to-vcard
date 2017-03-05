434a48847c6ca890e0c515cad45134d939e020c1
James Benner <james.benner@gmail.com>
2017-03-04 20:22:21 -0500

    Replaced JSHint and JSCS with ESLint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,14 @@
 'use strict';
 
-let gulp   = require('gulp');
-let jscs   = require('gulp-jscs');
-let jshint = require('gulp-jshint');
-let micro  = require('gulp-micro');
+const eslint = require('gulp-eslint');
+const gulp   = require('gulp');
+const micro  = require('gulp-micro');
 
 gulp.task('js', () => {
     return gulp.src('lib/**/*.js')
         .pipe(micro({limit: 2500}))
-        .pipe(jscs())
-        .pipe(jscs.reporter())
-        .pipe(jscs.reporter('fail'))
-        .pipe(jshint())
-        .pipe(jshint.reporter())
-        .pipe(jshint.reporter('fail'));
+        .pipe(eslint())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['js']);
